1e468678f3f9ecf75907349f55e8bc5cd2c88674
vbrantner <vbrantner@fastmail.com>
2018-08-26 11:55:06 +0200

    updated error handling

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const argv = require('minimist')(process.argv.slice(2), {
+  string: 'mongodbURL', // --mongodbURL
+  int: 'port', // --port
+  default: { mongodbURL: 'mongodb://localhost/feedback', port: 3000 },
+});
 const routes = require('./routes/api');
 
 // using express
 const app = express();
 
-mongoose.connect('mongodb://localhost/feedback', { useNewUrlParser: true });
+mongoose.connect(argv.mongodbURL, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  // websites to allow to connect
+  res.setHeader('Acces-Control-Allow-Origin', '*');
+
+  // requests methods to allow
+  res.setHeader('Acces-Control-Allow-Methods', 'GET, POST');
+  next();
+});
+
 // using routes
 app.use('/api', routes);
 
-// error handling
+// error handling for not routed requests
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+// error handling middleware
 app.use((err, req, res, next) => {
-  res.status(422).send({ error: err.message });
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
+  next();
 });
 
 // listen for requests
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(argv.port, () => {
+  console.log(`App is listening on port ${argv.port}`);
+  console.log(`conntected to ${argv.mongodbURL}`);
 });
